b5d0fe0fcdeb1753f27eb5647d7d7a3d5c2bf0fd
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-08-31 00:59:24 +0900

    강의03 : padding, margin 적용(.fromLTRB)

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          width: 100,
-          height: 100,
-          color: Color.fromARGB(100, 10, 10, 10),
-          margin: EdgeInsets.all(20), // 외부 여백
-          padding: EdgeInsets.all(20), // 내부 여백
+          width: 100, height: 100, color: Color.fromARGB(100, 10, 10, 10),
+          margin: EdgeInsets.fromLTRB(20, 0, 0, 0), // 외부 여백
+          padding: EdgeInsets.fromLTRB(20, 0, 0, 0), // 내부 여백
           child: Text('여백 확인용 text'),
         ),
       )
