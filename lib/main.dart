00c16e26f5069bf28d85e80be204375d8791719e
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-09-01 17:15:04 +0900

    강의06 : 커스텀 위젯 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -25,39 +25,53 @@ class MyApp extends StatelessWidget {
         body: Container(
           width: 450, height: 150, color: Colors.black12,
           padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/memory_logo.png', width: 150, height: 150,),
-              Flexible( flex: 1,
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                        ),),
-                      Row(
-                        children: [
-                          Text('성동구 행당동'),
-                          Text(' • '),
-                          Text('끌올 10분 전')
-                        ],
-                      ),
-                      Text('210,000원'),
-                      Row(mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.heart_broken),
-                          Text('4')
-                        ],)
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+          child: a(),
         ),
       ),
+    );
+  }
+}
+
+
+
+
+
+
+class a extends StatelessWidget {
+  const a({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset('assets/memory_logo.png', width: 150, height: 150,),
+        Flexible( flex: 1,
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),),
+                Row(
+                  children: [
+                    Text('성동구 행당동'),
+                    Text(' • '),
+                    Text('끌올 10분 전')
+                  ],
+                ),
+                Text('210,000원'),
+                Row(mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(Icons.heart_broken),
+                    Text('4')
+                  ],)
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
