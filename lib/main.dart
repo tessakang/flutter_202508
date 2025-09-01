d4edef2204d2924e2be3be02b2555e0990b0fa03
KANG JIHYE <tessa.kangjihye@gmail.com>
2025-09-01 18:17:44 +0900

    강의06 : ListView

--- lib/main.dart
+++ lib/main.dart
@@ -12,66 +12,63 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Container(
-            child: Row(
-              children: [
-                Text('금도홍 3가', style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                ),),
-                Icon(Icons.add, size: 20,)
-              ],
-            )
-        ), actions: [Icon(Icons.saved_search, size: 30,), Icon(Icons.list_outlined, size: 30,), Icon(Icons.add_alert, size: 30,)], backgroundColor: Colors.green,),
-        body: Container(
-          width: 450, height: 150, color: Colors.black12,
-          padding: EdgeInsets.all(10),
-          child: a(),
+        appBar: AppBar(title: Text('앱바')),
+        body: ListView(
+          children: [
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+            Text('당케벨~'),
+          ],
         ),
-      ),
+      )
     );
   }
 }
 
-
-
-
-
-
-class a extends StatelessWidget {
-  const a({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset('assets/memory_logo.png', width: 150, height: 150,),
-        Flexible( flex: 1,
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                  ),),
-                Row(
-                  children: [
-                    Text('성동구 행당동'),
-                    Text(' • '),
-                    Text('끌올 10분 전')
-                  ],
-                ),
-                Text('210,000원'),
-                Row(mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.heart_broken),
-                    Text('4')
-                  ],)
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
